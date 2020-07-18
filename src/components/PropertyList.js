5e0a61e051af246d5e745079812077722aab9d4f
Juan Martín García <hello@juangarcia.com.ar>
2020-07-18 15:56:54 -0300

    Basic Styles for buscador

--- src/components/PropertyList.js
+++ src/components/PropertyList.js
@@ -1,19 +1,25 @@
 import React from 'react';
 
+import styles from './PropertyList.module.css';
+
 class PropertyList extends React.Component {
   render() {
     return (
-      <section className="properties">
-        <ul className="property-list">
+      <section className={styles.properties}>
+        <ul className={styles.propertyList}>
           {
             this.props.properties.map(
               (property, key) =>
               <li key={key}>
-                <a href={ property.url }>
-                  <img src={property.main_image} alt={property.title} />
-                  <h2>{property.title}</h2>
-                  <p>{property.description}</p>
-                  <p>Precio: {property.currency_id} {property.price}</p>
+                <a href={ property.url } target="_blank" rel="noopener noreferrer">
+                  <div className={styles.imageContainer}>
+                    <span>{property.currency_id} ${property.price}</span>
+                    <img src={property.main_image} alt={property.title} />
+                  </div>
+                  <div className={styles.textContainer}>
+                    <h2>{property.title}</h2>
+                    <p>{property.description}</p>
+                  </div>
                 </a>
               </li>
             )
